017bf41aa7bae7c106f164aa87fac74aad11749f
ivan.kopeykin <ivan.kopeykin@gmail.com>
2018-02-06 17:40:52 +0300

    feat(Editor): null when text is empty, 0.1.7

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -180,6 +180,11 @@ export default class Editor extends Component {
     handleChange() {
         if (this.props.onChange) {
             try {
+                const text = this.jsonEditor.getText();
+                if (text === '') {
+                    this.props.onChange(null);
+                }
+
                 const currentJson = this.jsonEditor.get();
                 if (this.props.value !== currentJson) {
                     this.props.onChange(currentJson);
